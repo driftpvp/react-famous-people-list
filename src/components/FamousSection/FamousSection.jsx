244289eedd2be87b12238d4be036d149422e1ef5
Lucas Martin <driftpvp@gmail.com>
2023-08-10 21:04:51 -0500

    required features coded. Would have like to clear the dynamic <p> field on submit with the form

--- src/components/FamousSection/FamousSection.jsx
+++ src/components/FamousSection/FamousSection.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './FamousSection.css';
+import axios from 'axios';
 
 function FamousSection() {
   let [famousPersonName, setPersonName] = useState('');
@@ -10,14 +11,34 @@ function FamousSection() {
 
   const fetchPeople = () => {
     // TODO: fetch the list of people from the server
+    axios.get('/people')
+    .then((response) => {
+      console.log(response.data);
+      setPeopleArray(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
   }
 
-  const addPerson = (evt) => {
-    evt.preventDefault();
+  useEffect(() => {
+    fetchPeople()
+  }, [])
+
+  const addPerson = (event) => {
+    event.preventDefault();
     console.log(`The person is ${famousPersonName} and they're famous for ${famousPersonRole}`);
     
     // TODO: create POST request to add this new person to the database
-
+    axios.post('/people', {name: famousPersonName, role: famousPersonRole})
+    .then((response) => {
+      event.target.reset();
+      fetchPeople()
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
     // HINT: the server is expecting a person object 
     //       with a `name` and a `role` property
   
@@ -37,6 +58,9 @@ function FamousSection() {
         </p>
         <ul>
           {/* TODO: Render the list of famous people */}
+          {famousPeopleArray.map(person => 
+      (<li key={person.name}>{person.name} is famous for {person.role}</li>))}
+
         </ul>
       </section>
     );
